8cbdb86a20c37d65e28e9b8b49823a5587f4f43d
morganluke <morganluke@gmail.com>
2022-05-21 20:12:52 -0700

    working on header added favicon

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -2,3 +2,13 @@ document.querySelector("#update").innerHTML = document.lastModified;
 
 const date = new Date();
 document.querySelector("#currentDate").innerHTML = date.getFullYear()
+
+document.querySelector("#date").innerHTML = date.getDay();
+
+function toggleMenu () {
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
