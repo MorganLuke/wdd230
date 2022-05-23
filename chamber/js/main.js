6a49c4bcaa991a9f0f97a3d550e57e20f075ad44
morganluke <morganluke@gmail.com>
2022-05-23 03:15:05 -0700

    completed chamber layout

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,10 +1,14 @@
+// last modified date
 document.querySelector("#update").innerHTML = document.lastModified;
 
+// copy year
 const date = new Date();
 document.querySelector("#currentDate").innerHTML = date.getFullYear()
 
-document.querySelector("#date").innerHTML = date.getDay();
+// todays date
+document.querySelector("#date").innerHTML = date.toLocaleString('default', {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'});
 
+// hamburger button
 function toggleMenu () {
     document.getElementById('primaryNav').classList.toggle('open');
     document.getElementById('hamburgerBtn').classList.toggle('open');
