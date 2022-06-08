a5034b59abc2f8fa1e21ccc6bb9fc7dc5516393c
morganluke <morganluke@gmail.com>
2022-06-08 00:49:26 -0700

    added responsive images to hero added windchill js calculations in weather

---
+++ chamber/js/windchill.js
@@ -0,0 +1,21 @@
+
+
+let temperature = parseFloat(document.querySelector("#temperature").textContent);
+let windspeed = parseFloat(document.querySelector("#windspeed").textContent);
+let windchill = "";
+
+
+if (temperature <= 50 && windspeed > 3) {
+  windchill = windChill(temperature, windspeed);
+  windchill = `${windchill}&#176;F`;
+} 
+else {
+  windchill = "N/A";
+}
+// output
+document.querySelector("#windchill").innerHTML = windchill;
+
+
+function windChill(temp, speed) {
+  return Math.round(35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16)));
+}
