9e8e0a225603f2d9adfe8ac2b85f4ccc09f2973b
morganluke <morganluke@gmail.com>
2022-06-13 14:10:06 -0700

    addedc close button to banner-popup

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -17,11 +17,20 @@ function toggleMenu () {
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
+
 // banner popup
 let dayOfWeek = date.getDay();
 // if date lands on monday or tuesday then the banner is displayed
-if (dayOfWeek == 1 || dayOfWeek == 2) {document.querySelector('#banner-popup').style.display = 'block';}
+if (dayOfWeek == 1 || dayOfWeek == 2) {document.querySelector('#banner-popup').style.display = 'flex';}
 else {document.querySelector('#banner-popup').style.display = 'none';};
+
+const close = document.querySelector("#close");
+const banner = document.querySelector("#banner-popup");
+
+close.addEventListener("click", () => {
+	banner.style.display = "none";
+});
+
 
 
 
