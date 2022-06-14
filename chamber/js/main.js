5859a403989c87365df574cb29f69b5dd8f0ac84
morganluke <morganluke@gmail.com>
2022-06-13 20:22:04 -0700

    completed form on join page created thank you content

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -18,18 +18,7 @@ const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
 
-// banner popup
-let dayOfWeek = date.getDay();
-// if date lands on monday or tuesday then the banner is displayed
-if (dayOfWeek == 1 || dayOfWeek == 2) {document.querySelector('#banner-popup').style.display = 'flex';}
-else {document.querySelector('#banner-popup').style.display = 'none';};
 
-const close = document.querySelector("#close");
-const banner = document.querySelector("#banner-popup");
-
-close.addEventListener("click", () => {
-	banner.style.display = "none";
-});
 
 
 
