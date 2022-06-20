369a2e350f321ae1a57e83bba1d8d9f1bc7eced9
morganluke <morganluke@gmail.com>
2022-06-20 15:13:56 -0700

    created directory page

---
+++ chamber/js/businesscards.js
@@ -0,0 +1,66 @@
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
+
+// Business Cards
+const URL = 'https://morganluke.github.io/wdd230/chamber/json/data.json';
+const cards = document.querySelector('.cards');
+
+async function getBusinesses() {
+    let response = await fetch(URL);
+    if (response.ok) {
+        let data = await response.json();
+        //console.log(data);
+        buildBusinessCards(data);
+    }
+    else {
+        throw Error(response.statusText);
+    }
+}
+
+function buildBusinessCards(data) {
+    data.businesses.forEach(business => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let img = document.createElement('img');
+                
+        h2.innerHTML = `${business.name}`;
+        p.innerHTML = `${business.address}`;
+        p2.innerHTML = `${business.phone}`;
+        p3.innerHTML = `${business.website}`;
+                
+        img.setAttribute('src', business.imageurl);
+        img.setAttribute('alt', `${business.name} business logo`);
+        img.setAttribute('loading', 'lazy');
+                
+        card.append(img);
+        card.append(h2);
+        card.append(p);
+        card.append(p2);
+        card.append(p3);
+
+                
+        cards.append(card);
+    });
+}
+
+getBusinesses();
